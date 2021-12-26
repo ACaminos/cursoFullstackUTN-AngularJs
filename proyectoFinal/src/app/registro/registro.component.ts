df1d15b5c384d90cee728b1ae3463c1840575add
Ariel Caminos <caminosariel@gmail.com>
2021-12-26 00:31:45 -0300

    new changes on unit 2

--- proyectoFinal/src/app/registro/registro.component.ts
+++ proyectoFinal/src/app/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,7 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  formulario:FormGroup
+
+  constructor(
+    private fb:FormBuilder
+  ) { 
+    this.formulario= this.fb.group({
+      nombre: ["", [Validators.required, Validators.minLength(3)]],
+      apellido: ["", [Validators.required, Validators.minLength(3)]],
+      tel: [""],
+      mail: ["", [Validators.required, Validators.minLength(3)]],
+      password: ["", [Validators.required, Validators.minLength(6), Validators.maxLength(10)]]
+    })
+  }
+
+  registrarse(){
+    console.log(this.formulario.value)
+  }
 
   ngOnInit(): void {
   }
